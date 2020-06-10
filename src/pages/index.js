3b3cfaca533a47e465093625fa649a26bc4f9813
Varun Raval <44644972+RavalVarun@users.noreply.github.com>
2020-06-10 16:35:08 -0400

    add basic styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { linke, Link } from 'gatsby';
+import { Link } from 'gatsby';
+import Layout from '../components/layout';
 
 export default () => (
-  <>
+  <Layout>
     <h1>Home</h1>
     <p>Hello Toronto</p>
     <Link to="/about">About me</Link>
-  </>
+  </Layout>
 );
